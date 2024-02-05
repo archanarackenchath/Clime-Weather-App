141cad1ed0c47ac6252c0ef822867479ca6d1c7b
archanarackenchath <archanarackenchath@gmail.com>
2024-02-05 12:49:02 +0530

    Flutter app created

---
+++ Flutter_Weather_App-master/lib/consts/strings.dart
@@ -0,0 +1,6 @@
+const String degree = "°";
+const String apiKey = "479da2286d31b187644e25bcafbcb4d4";
+const String currentEndpoint =
+    "https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API key}";
+const String daysEndpoint =
+    "api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}";
